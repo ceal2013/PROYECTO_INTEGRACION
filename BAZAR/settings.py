d99b63de6f8af5df1900acdaf4c035696bd2a9b0
ceal2013 <carlos.ortiz51@inacapmail.cl>
2026-02-11 17:43:55 -0300

    Mejorar la configuración de archivos estáticos: ajustar STATIC_URL y habilitar compresión con WhiteNoise

--- BAZAR/settings.py
+++ BAZAR/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,11 +123,15 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+#STATIC_URL = 'static/'
+STATIC_URL = '/static/'  # <--- Con la barra al inicio
 STATICFILES_DIRS = [STATIC_DIR]
 
 # Carpeta donde se guardarán los estáticos al usar "collectstatic" (Requerido para producción)
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Habilita la compresión y el cacheo eficiente de WhiteNoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
